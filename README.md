5cbbd58d3721f57fd091b0b86640f180cf2cae26
e <samuels.erika@yahoo.com>
2021-04-03 18:09:52 -0400

    update dependencies and readme

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 *NodeJS service using the express framework that implements a set of RESTful style interfaces and tests*
 
 [1] install node, npm, mongoDB<br/>
-[2] clone or fork repo onto your local machine<br/>
-[3] cd into project directory, `npm i`<br/>
-[4] to start, run `npm start` and hit api routes at `localhost:9000` defined in `server/api`<br/>
-[5] to test the entire suite, run `npm test`, or `mocha -g <describe_block_text>` to run a single test<br/>
+[2] start mongodb `sudo systemctl start mongod`
+[3] clone or fork repo onto your local machine<br/>
+[4] cd into project directory, `npm i`<br/>
+[5] to start, run `npm start` and hit api routes at `localhost:9000` defined in `server/api`<br/>
+[6] to test the entire suite, run `npm test`, or `mocha -g <describe_block_text>` to run a single test<br/>
--- server/start.js
+++ server/start.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
 const PORT = process.env.PORT || 9000;
 const app = express();
-const {connection} = require('../db');
-const {stream} = require('../utility');
+const { connection } = require("../db");
+const { stream } = require("../utility");
 
 const init = async () => {
   app
-  .use(morgan('combined', {stream}))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use('/api', require('./api'))
+    .use(morgan("combined", { stream }))
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use("/api", require("./api"));
 
   await connection;
 
   let server = http.createServer(app);
   server.listen(PORT, () => {
-    console.log(`listening on port ${server.address().port}!`)
+    console.log(`listening on port ${server.address().port}!`);
   });
 };
 
@@ -26,5 +25,5 @@ init();
 
 module.exports = {
   PORT,
-  app
+  app,
 };
